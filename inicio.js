20c4988414d5191c1ed7678dc852882df65a1039
eldarkp123 <leystonpinela@gmail.com>
2024-09-11 14:57:20 -0500

    Update inicio.js

--- inicio.js
+++ inicio.js
@@ -1,31 +1,3 @@
-// Variables para el usuario y la contraseña correctos
-const correctUsername = "usercolegio1";
-const correctPassword = "colegio123";
-
-// Capturar el formulario y el mensaje de error
-const loginForm = document.getElementById('loginForm');
-const loginMessage = document.getElementById('loginMessage');
-
-// Validación del formulario de inicio de sesión
-loginForm.addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-
-    // Validación de credenciales
-    if (username === correctUsername && password === correctPassword) {
-        loginMessage.style.color = "green";
-        loginMessage.innerHTML = "Inicio de sesión exitoso. Redirigiendo...";
-        loginMessage.style.display = "block";
-        
-        // Redirigir después de 1 segundo
-        setTimeout(() => {
-            window.location.href = "cursos.html"; // Aquí rediriges a la página principal
-        }, 1000);
-    } else {
-        loginMessage.style.color = "#ff4d4d";
-        loginMessage.innerHTML = "Usuario o contraseña incorrectos.";
-        loginMessage.style.display = "block";
-    }
-});
+function avanzarPagina() {
+    window.location.href = "cursos.html"; // Cambia esta página por la siguiente que prefieras
+}
